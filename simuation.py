2f496e03a31aab5b587ca3d1eb54e283833407e9
myprojectsgithub <derp.thiago@gmail.com>
2021-01-11 21:19:20 -0300

    Added some basic pygame

--- simuation.py
+++ simuation.py
@@ -1,1 +1,17 @@
 # File to Set the Graphic Interface
+
+import pygame
+
+# pylint: disable=no-member
+pygame.init()
+# pylint: enable=no-member
+
+screen = pygame.display.set_mode((800,600))
+
+running = True
+while(running):
+     for event in pygame.event.get():
+         # pylint: disable=no-member
+         if event.type == pygame.QUIT:
+        # pylint: enable=no-member
+             running = False
